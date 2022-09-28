baddb1fd22314d80db5a071c1e57ac85ce6dddec
YuzuruOtonashi <102186286+YuzuruOtonashi@users.noreply.github.com>
2022-09-27 22:49:18 -0300

    Header

--- ProyectoIntegrador_MartinezDiego/src/app/app.module.ts
+++ ProyectoIntegrador_MartinezDiego/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoApComponent
   ],
   imports: [
     BrowserModule,
