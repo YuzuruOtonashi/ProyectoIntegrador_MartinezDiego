b144803b52de7ae140ef424ac46d43b66613a13f
YuzuruOtonashi <102186286+YuzuruOtonashi@users.noreply.github.com>
2022-10-01 23:39:30 -0300

    Hard & Soft Skills

--- ProyectoIntegrador_MartinezDiego/src/app/app.module.ts
+++ ProyectoIntegrador_MartinezDiego/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
 
 @NgModule({
   declarations: [
@@ -20,11 +22,13 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    HysComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
