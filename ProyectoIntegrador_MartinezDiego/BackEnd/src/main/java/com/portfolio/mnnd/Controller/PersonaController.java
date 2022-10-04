c57e521b71e40044bc32e722373424d41830c4a3
YuzuruOtonashi <102186286+YuzuruOtonashi@users.noreply.github.com>
2022-10-04 14:40:24 -0300

    Correcciones

--- ProyectoIntegrador_MartinezDiego/BackEnd/src/main/java/com/portfolio/mnnd/Controller/PersonaController.java
+++ ProyectoIntegrador_MartinezDiego/BackEnd/src/main/java/com/portfolio/mnnd/Controller/PersonaController.java
@@ -29,13 +29,13 @@ public class PersonaController {
         return "La persona fue creada correctamente";
     }
     
-    @DeleteMapping("/personas/borrar/(id)")
+    @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaservice.deletePersona(id);
         return "La persona fue eliminada correctamente";
     }
     
-    @PutMapping("/persona/editar/(id)")
+    @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id, 
                               @RequestParam("nombre") String nuevoNombre,
                               @RequestParam("apellido") String nuevoApellido,
