a06fa3090adbd21dffee057cccda6b83080b90a6
YuzuruOtonashi <102186286+YuzuruOtonashi@users.noreply.github.com>
2022-10-03 21:32:07 -0300

    Add backend

---
+++ ProyectoIntegrador_MartinezDiego/BackEnd/src/main/java/com/portfolio/mnnd/MnndApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mnnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnndApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_MartinezDiego/BackEnd/src/test/java/com/portfolio/mnnd/MnndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mnnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
