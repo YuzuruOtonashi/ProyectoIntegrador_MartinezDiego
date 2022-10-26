495234f3621ab8fe1e9dfcff1a2d4c767d7a15bc
YuzuruOtonashi <102186286+YuzuruOtonashi@users.noreply.github.com>
2022-10-25 21:21:34 -0300

    JWT

---
+++ ProyectoIntegrador_MartinezDiego/BackEnd/src/main/java/com/portfolio/mnnd/Security/Enums/RolNombre.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.mnnd.Security.Enums;
+
+/**
+ *
+ * @author mdieg
+ */
+public enum RolNombre {
+    ROLE_ADMIN, ROL_USER;
+}
